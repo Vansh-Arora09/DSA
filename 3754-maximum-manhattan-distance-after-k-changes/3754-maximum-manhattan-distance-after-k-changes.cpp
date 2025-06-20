dc334cd42d695472e359d580115bff6717b9b00d
Vansh-Arora09 <149749612+Vansh-Arora09@users.noreply.github.com>
2025-06-20 12:26:44 +0530

    Time: 53 ms (88.79%), Space: 38.1 MB (74.85%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
@@ -3,29 +3,24 @@ public:
     int maxDistance(string s, int k) {
         int N = s.length();
         int ans=0;
-        int n=0,w=0,e=0,st=0;
+        int lat = 0, lon = 0;;
         for(int i=0;i<N;i++){
             switch(s[i]){
                 case 'N':
-                    n++;
+                    lat++;
                     break;
                 case 'S':
-                    st++;
+                    lat--;
                     break;
                 case 'E':
-                    e++;
+                    lon++;
                     break;
                 case 'W':
-                    w++;
+                    lon--;
                     break;
             }
-            int t1 = min({n,st,k});
-            int t2 = min({e,w,k-t1});
-            ans = max(ans,count(n,st,t1)+count(e,w,t2));
+            ans = max(ans,min(abs(lat)+abs(lon)+k*2,i+1));
         }
         return ans;
-    }
-    int count(int d1,int d2,int times){
-        return abs(d1-d2)+times*2;
     }
 };
