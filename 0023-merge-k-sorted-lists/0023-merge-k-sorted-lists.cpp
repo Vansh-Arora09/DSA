3ec9b1b1f08d0e9181172ad31d3f1319fe70d62f
Vansh Arora <va325746@gmail.com>
2025-09-07 14:13:51 +0530

    Time: 0 ms (100%), Space: 18.7 MB (34.43%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,3 +1,4 @@
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,27 +11,25 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1,ListNode* l2){
+    ListNode* merge(ListNode* l1,ListNode* l2){
         if(!l1) return l2;
         if(!l2) return l1;
         if(l1->val < l2->val){
-            l1->next = mergeTwoLists(l1->next,l2);
+            l1->next = merge(l1->next,l2);
             return l1;
         }
         else{
-            l2->next = mergeTwoLists(l2->next,l1);
+            l2->next = merge(l2->next,l1);
             return l2;
         }
     }
-    ListNode* mergeLists(vector<ListNode*>&lists , int left,int right){
-        if(left==right) return lists[left];
-        if(left>right) return nullptr;
-
-        int mid = left + (right-left)/2;
-        ListNode* l1 = mergeLists(lists,left,mid);
-        ListNode* l2 = mergeLists(lists,mid+1,right);
-
-        return mergeTwoLists(l1,l2);
+    ListNode* mergeLists(vector<ListNode*>&lists, int low,int high){
+        if(low==high) return lists[low];
+        if(low>high) return nullptr;
+        int mid = low+(high-low)/2;
+        ListNode* l1 = mergeLists(lists,low,mid);
+        ListNode* l2 = mergeLists(lists,mid+1,high);
+        return merge(l1,l2);
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         return mergeLists(lists,0,lists.size()-1);
