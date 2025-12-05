44fb8383cfa05aa478f1e747ab32e4a367341ea8
Vansh Arora <va325746@gmail.com>
2025-12-05 21:01:52 +0530

    Time: 0 ms (100%), Space: 23.4 MB (5.3%) - LeetHub

---
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> arr(n);
+        arr[0] = nums[0];
+        for(int i=1;i<n;i++){
+            arr[i] = arr[i-1] + nums[i];
+        }
+
+        long long total=arr[n-1];
+        int cnt=0;
+        for(int i=0;i<n-1;i++){
+            long long left = arr[i];
+            long long right = total - left;
+            if((left%2+2)%2 == (right%2+2)%2){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
