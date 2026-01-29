8a4624f1f74c8679037c1d10d671bbad4e34a3c5
Vansh Arora <va325746@gmail.com>
2026-01-29 12:27:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (13.28%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long start = 0;
+        long long ans=0;
+        long long end = x;
+        while(start<=end){
+            long long mid = (start+end)/2;
+            if(mid*mid <= x){
+                ans = mid;
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+};
