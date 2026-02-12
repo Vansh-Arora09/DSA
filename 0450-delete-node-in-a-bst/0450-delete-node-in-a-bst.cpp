914eec976d67a913a0d409cf792f3318d6a82530
Vansh Arora <va325746@gmail.com>
2026-02-12 11:38:08 +0530

    Time: 0 ms (100%), Space: 34.3 MB (75.57%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -12,36 +12,32 @@
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root==nullptr) return nullptr;
+        if(!root) return NULL;
+
         if(root->val<key){
-            root->right = deleteNode(root->right,key);
+            root->right = deleteNode(root->right, key);
         }
         else if(root->val>key){
             root->left = deleteNode(root->left,key);
         }
         else{
-            if(root->left==NULL && root->right==NULL){
+            if(!root->left && !root->right){
                 return NULL;
             }
-
-            if(root->left==NULL){
-                return root->right;
+            else if(!root->left || !root->right){
+                return root->left ? root->left : root->right;
             }
-            else if(root->right==NULL){
-                return root->left;
+            else{
+                TreeNode* temp = root->right;
+                while(temp->left!=NULL){
+                    temp = temp->left;
+                }
+                root->val = temp->val;
+
+                root->right = deleteNode(root->right,temp->val);
             }
-
-            TreeNode* mini = find(root->right);
-            root->val = mini->val;
-
-            root->right = deleteNode(root->right,mini->val);
         }
         return root;
-    }
-    TreeNode* find(TreeNode* root){
-        while(root->left!=NULL){
-            root = root->left;
-        }
-        return root;
+
     }
 };
