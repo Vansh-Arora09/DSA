5775a794a3a61e5b9e2714649fc09016f1924be4
Vansh Arora <va325746@gmail.com>
2026-02-14 12:38:49 +0530

    Time: 14 ms (13.32%), Space: 45.7 MB (19.37%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> dp(101, vector<double>(101,0.0));
+
+        dp[0][0] = (double)poured;
+
+        for(int i=0;i<=query_row;i++){
+            for(int j=0;j<=i;j++){
+                if(dp[i][j]>1.0){
+                    double excess = dp[i][j] - 1.0;
+                    double spill = excess/2.0;
+
+                    dp[i+1][j]+=spill;
+                    dp[i+1][j+1]+=spill;
+
+                    dp[i][j] = 1.0;
+                }
+            }
+        }
+        return min(1.0, dp[query_row][query_glass]);
+
+    }
+};
