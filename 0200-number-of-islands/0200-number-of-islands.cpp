ece132798add0ccb487b7a5d192ccdfb4eb18794
Vansh Arora <va325746@gmail.com>
2026-02-28 16:29:05 +0530

    Time: 28 ms (49.41%), Space: 16.8 MB (58.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
 
-    void floodfill(int i, int j, vector<vector<char>>&grid, vector<vector<bool>>&vis){
+    void dfs(int i, int j , vector<vector<char>>& grid, vector<vector<bool>>&vis){
         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || vis[i][j] || grid[i][j]=='0'){
-            return;
+            return ;
         }
         vis[i][j] = true;
-        floodfill(i+1,j ,grid,vis);
-        floodfill(i-1,j,grid,vis);
-        floodfill(i, j+1,grid, vis);
-        floodfill(i, j-1, grid, vis);
+        dfs(i+1,j,grid,vis);
+        dfs(i-1,j,grid,vis);
+        dfs(i, j+1, grid, vis);
+        dfs(i, j-1, grid, vis);
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m,false));
         int cnt=0;
+        vector<vector<bool>> vis(n, vector<bool>(m,false));
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && visited[i][j]==false){
-                    floodfill(i,j, grid, visited);
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    dfs(i,j,grid,vis);
                     cnt++;
                 }
             }
