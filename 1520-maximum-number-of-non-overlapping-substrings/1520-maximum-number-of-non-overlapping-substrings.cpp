4abbcf40dff6a0f02910a6ec17c3a907111b98f0
Vansh Arora <va325746@gmail.com>
2026-03-01 17:36:19 +0530

    Time: 252 ms (12.72%), Space: 280.8 MB (10.95%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
 
-    void dfs(int node, vector<int> adj[], stack<int>&st, vector<int>& vis){
+    void dfs(int node, stack<int>&st, vector<int>&vis, vector<int> adj[]){
         vis[node] = true;
-        for(int neigh : adj[node]){
-            if(!vis[neigh]){
-                dfs(neigh,adj,st,vis);
+        for(auto ch : adj[node]){
+            if(!vis[ch]){
+                dfs(ch, st, vis, adj);
             }
         }
         st.push(node);
     }
 
-    void dfs2(int node, vector<int> adjT[], vector<int>&vis, vector<int>&scc, int id){
-        vis[node]=  true;
-        scc[node] = id;
-        for(int neigh : adjT[node]){
+    void dfs2(int node, vector<int>&scc, int cnt, vector<int>&vis, vector<int> adjT[] ){
+        vis[node] = true;
+        scc[node] = cnt;
+
+        for(auto neigh : adjT[node]){
             if(!vis[neigh]){
-                dfs2(neigh, adjT, vis,scc,id);
+                dfs2(neigh, scc, cnt, vis, adjT);
             }
         }
     }
     vector<string> maxNumOfSubstrings(string s) {
         int n = s.length();
-        vector<int> first(26,-1);
-        vector<int> last(26,-1);
+        //1. find first and last index of every character
+
+        vector<int> first(26,-1), last(26,-1);
+
         for(int i=0;i<n;i++){
             if(first[s[i]-'a']==-1){
                 first[s[i]-'a'] = i;
             }
             last[s[i]-'a'] = i;
         }
+
+        //2. build the adjacency vector
 
         vector<int> adj[26];
         vector<bool> exists(26,false);
@@ -39,19 +44,23 @@ public:
 
             exists[i] = true;
             for(int j = first[i];j<=last[i];j++){
-                if(s[j]-'a' !=i){
+                if(s[j]-'a'!=i){
                     adj[i].push_back(s[j]-'a');
                 }
             }
         }
 
+        //3. dfs1 + stack
         stack<int> st;
         vector<int> vis(26,0);
+
         for(int i=0;i<26;i++){
             if(exists[i] && !vis[i]){
-                dfs(i,adj, st, vis);
+                dfs(i, st, vis, adj);
             }
         }
+
+        //4. dfs2 + adjT
 
         vector<int> adjT[26];
         for(int i=0;i<26;i++){
@@ -65,11 +74,12 @@ public:
         int cnt=0;
         while(!st.empty()){
             int node = st.top(); st.pop();
+
             if(!vis[node]){
-                dfs2(node, adjT, vis, scc, cnt++);
+                dfs2(node, scc, cnt++, vis, adjT);
             }
         }
-
+        //5. out_degree vector
         vector<int> out_deg(cnt,0);
         for(int i=0;i<26;i++){
             if(!exists[i]) continue;
@@ -80,13 +90,16 @@ public:
                 }
             }
         }
-        vector<string> res;
-        for(int i = cnt-1;i>=0;i--){
-            if(out_deg[i]==0){
-                int left=n;
-                int right=-1;
 
-                for(int idx = 0;idx<26;idx++){
+        vector<string> res;
+
+        for(int i=cnt-1;i>=0;i--){
+            if(out_deg[i]==0){
+                //6. checking for left and rightmost index of a valid range
+                int left = n;
+                int right = -1;
+
+                for(int idx=0;idx<26;idx++){
                     if(scc[idx]==i){
                         left = min(left, first[idx]);
                         right = max(right, last[idx]);
@@ -95,12 +108,13 @@ public:
 
                 bool valid = true;
 
-                for(int k = left;k<=right;k++){
+                for(int k=left;k<=right;k++){
                     if(scc[s[k]-'a']!=i){
-                        valid=false;
+                        valid  = false;
                         break;
                     }
                 }
+                // add substring to res
                 if(valid){
                     res.push_back(s.substr(left, right-left+1));
                 }
