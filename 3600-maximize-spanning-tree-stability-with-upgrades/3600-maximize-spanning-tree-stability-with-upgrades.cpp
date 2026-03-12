6c033c06fdfcb533e8d134657e0c67617a5590c8
Vansh Arora <va325746@gmail.com>
2026-03-13 00:08:10 +0530

    Time: 132 ms (52.29%), Space: 266 MB (35.03%) - LeetHub

--- 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.cpp
+++ 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.cpp
@@ -1,3 +1,4 @@
+
 struct Edge{
     int u, v, w, type;
 };
@@ -6,20 +7,20 @@ class DSU{
     public:
     vector<int> parent;
 
-    DSU(vector<int> &p){
+    DSU(vector<int>&p){
         parent = p;
     }
+
     int find(int x){
         if(parent[x]==x){
             return x;
         }
         return parent[x] = find(parent[x]);
     }
-
     void join(int u, int v){
         int pu = find(u);
         int pv = find(v);
-        if(pu !=pv){
+        if(pu!=pv){
             parent[pu] = pv;
         }
     }
@@ -27,30 +28,24 @@ class DSU{
 class Solution {
 public:
     int maxStability(int n, vector<vector<int>>& edges, int k) {
-        // the strategy is to first add all the weights with must = 1
-        // if they form a cycle return -1
-        // then add maximum possible score from remaining edges i.e., edges with must=0 
-        // double the lowest k scores 
-        // minimize the ans for the final maximum minimum score
-
-        // HINT: -where this maximize minimize or minimize maximize concept is there, Binary Serach is the blind topic to apply
-
+        
+        
+        //do it agin for practice and to understand the concept of dsu and graph 
         int ans = -1;
+
         if(edges.size() < n-1){
             return -1;
         }
 
         vector<Edge> sorted;
         sorted.reserve(edges.size());
-
         for(auto &edge : edges){
-            sorted.push_back({edge[0], edge[1], edge[2], edge[3]});  // u, v, strength, must
+            sorted.push_back({edge[0],edge[1],edge[2],edge[3]});
         }
 
         vector<Edge> must;
         vector<Edge> optional;
-
-        for(auto &edge : sorted){
+        for(auto & edge : sorted){
             if(edge.type==1){
                 must.push_back(edge);
             }
@@ -58,73 +53,66 @@ public:
                 optional.push_back(edge);
             }
         }
-
         if(must.size() > n-1){
             return -1;
         }
 
-        sort(optional.begin(), optional.end(), [](Edge &a, Edge &b){
-            return b.w < a.w;
+        sort(optional.begin(),optional.end(),[](Edge &a, Edge &b){
+            return a.w > b.w;
         });
 
-        int selected=0;
+        int cnt=0;
         int stability = 2e9;
         vector<int> initial(n);
-        iota(initial.begin(), initial.end(), 0);
+        iota(initial.begin(), initial.end(),0);
         DSU dsu(initial);
-
+        
         for(auto &edge : must){
-            if(dsu.find(edge.u) == dsu.find(edge.v) || selected == n-1){
+            if(dsu.find(edge.u)==dsu.find(edge.v)){
                 return -1;
             }
-
             dsu.join(edge.u, edge.v);
-            selected++;
+            cnt++;
             stability = min(stability, edge.w);
         }
 
-        int l = 0;
-        int r = stability;
+        int l=0;
+        int r=stability;
 
         while(l<r){
-            int mid = l + ((r-l+1)>>1);
-
-            DSU dsumid(dsu.parent);
-
-            int slc = selected;
+            int mid = l + ((r-l+1)/2);
+            DSU dsu2(dsu.parent);
             int doublecnt=0;
+            int slc = cnt;
 
-            for(auto & edge : optional){
-                if(dsumid.find(edge.u)==dsumid.find(edge.v)){
+            for(auto &edge : optional){
+                if(dsu2.find(edge.u)==dsu2.find(edge.v)){
                     continue;
                 }
 
-                if(edge.w >=mid){
-                    dsumid.join(edge.u, edge.v);
+                if(edge.w>=mid){
+                    dsu2.join(edge.u, edge.v);
                     slc++;
                 }
-
-                else if(doublecnt<k && edge.w*2 >= mid){
+                else if(doublecnt < k && edge.w*2 >=mid){
+                    dsu2.join(edge.u, edge.v);
                     doublecnt++;
-                    dsumid.join(edge.u, edge.v);
                     slc++;
                 }
-                else{
-                    break;
-                }
-
+                
                 if(slc==n-1){
                     break;
                 }
             }
             if(slc!=n-1){
-                r  =mid-1;
+                r = mid-1;
             }
             else{
                 ans = mid;
                 l = mid;
             }
         }
+        
         return ans;
     }
 };
