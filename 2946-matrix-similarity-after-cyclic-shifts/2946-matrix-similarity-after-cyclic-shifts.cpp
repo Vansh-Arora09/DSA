13f04e6c7f2f97526e6f5618addc4e8235dd8d35
Vansh Arora <va325746@gmail.com>
2026-03-27 15:53:00 +0530

    Time: 0 ms (100%), Space: 31.4 MB (49.69%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
-    bool areSimilar(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        vector<vector<int>> mat = grid;
-        while(k--){
-            for(int i=0;i<m;i+=2){
-                int first = mat[i][0];
-                for(int j=1;j<n;j++){
-                    mat[i][j-1] = mat[i][j];
-                }
-                mat[i][n-1] = first;
-            }
-            for(int i=1;i<m;i+=2){
-                int last = mat[i][n-1];
-                for(int j=n-1;j>0;j--){
-                    mat[i][j] = mat[i][j-1];
-                }
-                mat[i][0] = last;
-            }
-        }
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+        k%=n;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]!=grid[i][j]){
+                if(mat[i][j] != mat[i][(j+k)%n]){
                     return false;
                 }
             }
-        }
+        }    
         return true;
     }
 
