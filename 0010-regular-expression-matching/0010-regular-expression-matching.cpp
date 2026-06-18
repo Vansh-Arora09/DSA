35f3fd6743aa05167aac195f507078ef4ec44e70
Vansh Arora <va325746@gmail.com>
2026-06-18 14:04:18 +0530

    Time: 15 ms (14.26%), Space: 21.2 MB (5.02%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    bool dfs(string s, string p, int i , int j){
-        if(j==p.length()){
-            return i==s.length();
-        }
+    bool isMatched(string s, string p, int i, int j){
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        bool curr = (i<s.length() && (s[i]==p[j] || p[j]=='.'));
-        bool ans=false;
+        if(j==p.length()){
+            return i==s.length();
+        }
 
-        if( j+1<p.length() && p[j+1]=='*'){
-            ans = dfs(s,p,i,j+2) || (curr && dfs(s,p,i+1,j));
+        bool current = (i<s.length() && (s[i]==p[j] || p[j]=='.'));
+        bool ans=0;
+
+        if(j+1<p.length() && (p[j+1]=='*')){
+            ans = isMatched(s,p,i,j+2) || (current && isMatched(s,p,i+1,j));
         }
         else{
-            ans = curr && dfs(s,p,i+1,j+1);
+            ans = (current && isMatched(s,p,i+1,j+1));
         }
-        return dp[i][j] = ans ? 1:0;
+        return dp[i][j] = ans ? 1 : 0;
     }
     bool isMatch(string s, string p) {
-        dp.assign(s.length()+1, vector<int>(p.length()+1, -1));
-
-        return dfs (s,p,0,0);
+        int n = s.length();
+        int m = p.length();
+        dp.assign(n+1, vector<int>(m+1,-1));
+        return isMatched(s,p,0,0);
     }
 };
