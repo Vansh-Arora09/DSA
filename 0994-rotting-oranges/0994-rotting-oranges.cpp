7e5955f9004b38d2d6aececf52eeb83e9d2250b4
Vansh Arora <va325746@gmail.com>
2026-06-29 11:00:51 +0530

    Time: 6 ms (6.64%), Space: 16.8 MB (70.18%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
+    void rotten(vector<vector<int>>&grid, int i, int j){
+        
+    }
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> timerot(n, vector<int>(m,0));
         queue<pair<int,int>> q;
+        int fresh=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
                 }
                 else if(grid[i][j]==1){
-                    timerot[i][j] = 1e8;
+                    fresh++;
                 }
             }
         }
 
-        while(q.size()){
-            pair<int,int> curr = q.front();
-            q.pop();
-            int i = curr.first;
-            int j = curr.second;
-            if(i+1<n && timerot[i+1][j]==1e8){
-                timerot[i+1][j] = 1 + timerot[i][j];
-                q.push({i+1, j});
+        int minutes=0;
+        if(fresh==0) return 0;
+
+        int drow[] = {-1,1,0,0};
+        int dcol[] = {0,0,-1,1};
+
+        while(!q.empty()){
+            int size = q.size();
+            bool get_rotten=false;
+
+            for(int i=0;i<size;i++){
+                auto [r,c] = q.front();
+                q.pop();
+
+                for(int d=0;d<4;d++){
+                    int nrow = r + drow[d];
+                    int ncol = c + dcol[d];
+
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1){
+                        grid[nrow][ncol] = 2;
+                        q.push({nrow, ncol});
+                        fresh--;
+                        get_rotten=true;
+                    }
+                }
             }
-            if(j+1<m && timerot[i][j+1]==1e8){
-                timerot[i][j+1] = 1 + timerot[i][j];
-                q.push({i, j+1});
-            }
-            if(i-1>=0 && timerot[i-1][j]==1e8){
-                timerot[i-1][j] = 1 + timerot[i][j];
-                q.push({i-1, j});
-            }
-            if(j-1>=0 && timerot[i][j-1]==1e8){
-                timerot[i][j-1] = 1 + timerot[i][j];
-                q.push({i, j-1});
-            }
+            if(get_rotten) minutes++;
         }
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans = max(ans, timerot[i][j]);
-            }
-        }
-        return (ans==1e8 ?  -1: ans);
+        return fresh==0 ? minutes : -1;
     }
 };
