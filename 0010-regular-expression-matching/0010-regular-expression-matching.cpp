1d7124eec1cfb3c1976176aea67abf2bcf6ae75b
Vansh Arora <va325746@gmail.com>
2026-07-26 18:22:52 +0530

    Time: 0 ms (100%), Space: 9.6 MB (18.66%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    bool isMatched(string s, string p, int i, int j){
+    bool Check(string &s, string &p, int i, int j){
+        if(j==p.length()){
+            //that means end of the string , no more checking in s ;
+            return s.length()==i;
+        }
+
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(j==p.length()){
-            return i==s.length();
-        }
 
-        bool current = (i<s.length() && (s[i]==p[j] || p[j]=='.'));
-        bool ans=0;
-
-        if(j+1<p.length() && (p[j+1]=='*')){
-            ans = isMatched(s,p,i,j+2) || (current && isMatched(s,p,i+1,j));
+        bool curr = (i<s.length() && (s[i]==p[j] || p[j]=='.'));
+        int ans=0;
+        if(j+1<p.length() && p[j+1]=='*'){
+            ans = curr && Check(s,p,i+1,j) || Check(s,p,i,j+2);
         }
         else{
-            ans = (current && isMatched(s,p,i+1,j+1));
+            ans = curr && Check(s,p,i+1,j+1);
         }
-        return dp[i][j] = ans ? 1 : 0;
+        return dp[i][j] = ans ? 1:0;
     }
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
         dp.assign(n+1, vector<int>(m+1,-1));
-        return isMatched(s,p,0,0);
+        return Check(s,p,0,0);
     }
 };
