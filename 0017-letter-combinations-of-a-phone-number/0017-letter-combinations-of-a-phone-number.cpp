4fdf8a74a48e9ad3c4edf571dee74c18951b10de
Vansh Arora <va325746@gmail.com>
2026-07-26 17:59:04 +0530

    Time: 2 ms (15.99%), Space: 9.5 MB (21.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -3,31 +3,24 @@ public:
     vector<string> ans;
     string curr="";
     unordered_map<char, string> mpp{
-        {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
-        {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
+        {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8',"tuv"},{'9', "wxyz"}
     };
-
-    void gencomb(string digits, int idx){
-        if(idx==digits.size()){
+    void Comb(string &digits, int i){
+        if(i==digits.size()){
             ans.push_back(curr);
-            return;
+            return ;
         }
-        string possible = mpp[digits[idx]];
-
-        for(char ch : possible){
-            curr.push_back(ch);
-            gencomb(digits,idx+1);
+        string valid = mpp[digits[i]];
+        for(char c : valid){
+            curr.push_back(c);
+            Comb(digits,i+1);
             curr.pop_back();
         }
-
     }
     vector<string> letterCombinations(string digits) {
         if(digits.empty()) return {};
 
-        ans.clear();
-        curr="";
-        gencomb(digits,0);
-        return ans;        
-        
+        Comb(digits,0);
+        return ans;
     }
 };
