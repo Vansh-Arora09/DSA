71f094e3c75b067714ea0f18b30bc26e65be4028
Vansh Arora <va325746@gmail.com>
2026-08-06 23:39:41 +0530

    Time: 3 ms (55.27%), Space: 25.9 MB (5.5%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        bool rows[9][9] = {false};
-        bool cols[9][9] = {false};
-        bool boxes[9][9] = {false};
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    int boxIndex = (i / 3) * 3 + (j / 3);
-                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) return false;
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+        
+        unordered_set<int> row[9], col[9], box[9];
+        for(int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
+                char val = board[r][c];
+                if(val=='.') continue;
+                int box_ind = (r/3) *3 + (c/3);
+                if(row[r].count(val) || col[c].count(val) || box[box_ind].count(val)){
+                    return false; // not possible, duplicate already exists.
                 }
+                row[r].insert(val);
+                col[c].insert(val);
+                box[box_ind].insert(val);
             }
         }
         return true;
