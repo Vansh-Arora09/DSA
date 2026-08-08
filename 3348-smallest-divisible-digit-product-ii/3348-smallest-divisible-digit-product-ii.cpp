5e824a35674954fa981f9aac18df39162ed32286
Vansh Arora <va325746@gmail.com>
2026-08-08 09:56:44 +0530

    Time: 29 ms (75.33%), Space: 46.8 MB (55.33%) - LeetHub

---
+++ 3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string smallestNumber(string num, long long t) {
+        long long tmp = t;
+        for(int i=2;i<=9;i++){
+            while(tmp%i==0){
+                tmp/=i;
+            }
+        }
+        if(tmp>1) return "-1";
+
+        int n = num.length();
+        vector<long long> rem(n+1);
+
+        rem[0] = t;
+        int pos = n-1;
+        for(int i=0;i<n;i++){
+            if(num[i]=='0'){
+                pos=i;
+                break;
+            }
+            rem[i+1] = rem[i]/gcd(rem[i], num[i]-'0');
+        }
+        if(rem[n]==1) return num;
+
+        for(int i=pos;i>=0;i--){
+            while(++num[i]<='9'){
+                long long tnow = rem[i]/gcd(rem[i],num[i]-'0');
+                int k=9;
+                for(int j=n-1;j>i;j--){
+                    while(tnow%k){
+                        k--;
+                    }
+                    tnow/=k;
+                    num[j] = '0'+k;
+                }
+                if(tnow==1) return num;
+            }
+        }
+
+        string ans;
+        for(int i=9;i>1;i--){
+            while(t%i==0){
+                ans+='0'+i;
+                t/=i;
+            }
+        }
+        ans+= string(max(n+1-(int)ans.length(),0),'1');
+        ranges:: reverse(ans);
+        return ans;
+    }
+};
