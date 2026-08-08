c5ce2425b716e24ef3fb28b0c84b86182607f50e
Vansh Arora <va325746@gmail.com>
2026-08-08 23:19:06 +0530

    Time: 9 ms (13.9%), Space: 12.2 MB (16.1%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    vector<vector<string>> res;
+    vector<vector<string>> ans;
     unordered_set<int> cols, diag1, diag2;
     void solve(int row, int n, vector<string>&board){
-        if(row==board.size()){
-            res.push_back(board);
+        if(row==n){
+            ans.push_back(board);
             return;
         }
         for(int i=0;i<n;i++){
@@ -14,13 +14,12 @@ public:
             if(cols.count(i) || diag1.count(d1) || diag2.count(d2)){
                 continue;
             }
-
             board[row][i] = 'Q';
             cols.insert(i);
             diag1.insert(d1);
             diag2.insert(d2);
 
-            solve(row+1,n,board);
+            solve(row+1, n, board);
 
             board[row][i] = '.';
             cols.erase(i);
@@ -28,9 +27,10 @@ public:
             diag2.erase(d2);
         }
     }
+
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n, string(n,'.'));
+        vector<string> board(n,string(n,'.'));
         solve(0, n, board);
-        return res;
+        return ans;
     }
 };
