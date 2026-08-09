c9e9ee1fb4d54573d7d66c4e41cff886aec2a0ab
Vansh Arora <va325746@gmail.com>
2026-08-09 16:29:02 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-int dp[10006] = {0};
+    vector<int> dp;
+    int solve(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = solve(n-1)+solve(n-2);
+    }
     int fib(int n) {
-        if(n<1) return 0;
-        if(n==1 || n==2) return 1;
-        if(dp[n]!=0){
-            //already calculated the state
-            return dp[n];
-        }
-        dp[n] = fib(n-1)+fib(n-2);
-
-        return dp[n];
+        dp.assign(n+1,-1);
+        return solve(n);
     }
 };
