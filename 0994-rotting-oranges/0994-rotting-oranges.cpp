ada5158fc5864979d1e0ea153c2a03e1c6217313
Vansh Arora <va325746@gmail.com>
2026-08-09 13:28:14 +0530

    Time: 0 ms (100%), Space: 16.8 MB (70.31%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,52 +1,42 @@
 class Solution {
 public:
-    void rotten(vector<vector<int>>&grid, int i, int j){
-        
-    }
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         queue<pair<int,int>> q;
         int fresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
                 }
-                else if(grid[i][j]==1){
-                    fresh++;
-                }
+                else if(grid[i][j]==1) fresh++;
             }
         }
-
-        int minutes=0;
         if(fresh==0) return 0;
 
-        int drow[] = {-1,1,0,0};
-        int dcol[] = {0,0,-1,1};
-
-        while(!q.empty()){
+        int minutes=0;
+        int dir[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+        while(!q.empty() && fresh>0){
             int size = q.size();
-            bool get_rotten=false;
+            minutes++;
 
             for(int i=0;i<size;i++){
                 auto [r,c] = q.front();
                 q.pop();
 
-                for(int d=0;d<4;d++){
-                    int nrow = r + drow[d];
-                    int ncol = c + dcol[d];
+                for(auto& d : dir){
+                    int nr = r+d[0];
+                    int nc = c+d[1];
 
-                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1){
-                        grid[nrow][ncol] = 2;
-                        q.push({nrow, ncol});
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1){
+                        grid[nr][nc]= 2;
                         fresh--;
-                        get_rotten=true;
+                        q.push({nr,nc});
                     }
                 }
             }
-            if(get_rotten) minutes++;
         }
-        return fresh==0 ? minutes : -1;
+        return fresh==0? minutes : -1;
     }
 };
