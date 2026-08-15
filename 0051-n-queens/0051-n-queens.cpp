25012cd599d9a658d522a0231f1aeb48c091343e
Vansh Arora <va325746@gmail.com>
2026-08-15 14:36:25 +0530

    Time: 8 ms (14.29%), Space: 12.3 MB (15.65%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,35 +2,34 @@ class Solution {
 public:
     vector<vector<string>> ans;
     unordered_set<int> cols, diag1, diag2;
-    void solve(int row, int n, vector<string>&board){
+    void solve(vector<string>&board, int row, int n){
         if(row==n){
             ans.push_back(board);
             return;
         }
-        for(int i=0;i<n;i++){
-            int d1 = row-i;
-            int d2 = row+i;
 
-            if(cols.count(i) || diag1.count(d1) || diag2.count(d2)){
-                continue;
-            }
-            board[row][i] = 'Q';
-            cols.insert(i);
+        for(int col=0;col<n;col++){
+            int d1 = row+col;
+            int d2 = row-col;
+
+            if(cols.count(col) || diag1.count(d1) || diag2.count(d2)) continue;
+
+            board[row][col] = 'Q';
+            cols.insert(col);
             diag1.insert(d1);
             diag2.insert(d2);
 
-            solve(row+1, n, board);
-
-            board[row][i] = '.';
-            cols.erase(i);
+            solve(board, row+1,n);
+            board[row][col]='.';
+            cols.erase(col);
             diag1.erase(d1);
             diag2.erase(d2);
         }
-    }
 
+    }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n,string(n,'.'));
-        solve(0, n, board);
-        return ans;
+        vector<string> board(n, string(n,'.'));
+        solve(board,0,n);
+        return ans; 
     }
 };
